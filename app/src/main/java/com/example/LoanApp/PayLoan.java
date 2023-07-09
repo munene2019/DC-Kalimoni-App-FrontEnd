6119ee27a069109478f094ab2a4f44e1efdef147
ADMIN <munenerogers@gmail.com>
2023-07-09 20:38:21 +0300

    uuu

--- app/src/main/java/com/example/LoanApp/PayLoan.java
+++ app/src/main/java/com/example/LoanApp/PayLoan.java
@@ -145,6 +145,8 @@ public class PayLoan extends AppCompatActivity {
             }
         });
 
+
+
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
